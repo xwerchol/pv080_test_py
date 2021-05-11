578d78c6a8913e08e258dc9e2a03f3e859cead01
xwerchol <492815@fi.muni.cz>
2021-05-11 16:52:26 +0200

    Update bad_pickle.py

--- bad_pickle.py
+++ bad_pickle.py
@@ -1,5 +1,6 @@
 # contains bunch of buggy examples
-# taken from https://hackernoon.com/10-common-security-gotchas-in-python-and-how-to-avoid-them-e19fbe265e03
+# taken from
+# https://hackernoon.com/10-common-security-gotchas-in-python-and-how-to-avoid-them-e19fbe265e03
 import cPickle
 import subprocess
 import base64
@@ -7,21 +8,39 @@ import subprocess
 
 
 # Input injection
-def transcode_file(request, filename):
+def transcode_file(filename):
+    """
+    transcode_file
+    :param filename: path to file
+    :return: nothing
+    """
     command = 'ffmpeg -i "{source}" output_file.mpg'.format(source=filename)
     subprocess.call(command, shell=True)  # a bad idea!
 
 
 # Assert statements
-def foo(request, user):
-   assert user.is_admin, 'user does not have access'
-   # secure code...
+def check_access(user):
+    """
+    checking access
+    :param user: username
+    :return: nothing
+    """
+    assert user.is_admin, 'user does not have access'
+    # secure code...
 
 
 # Pickles
 class RunBinSh(object):
-  def __reduce__(self):
-    return (subprocess.Popen, (('/bin/sh',),))
+    """
+    class RunBinSh
+    """
+
+    def __reduce__(self):
+        """
+        Reduction
+        :return: reduced process
+        """
+        return subprocess.Popen, (('/bin/sh',),)
 
 
 print(base64.b64encode(cPickle.dumps(RunBinSh())))
